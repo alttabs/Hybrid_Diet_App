e07ee3367794b86f76e6e47180000e496e6be944
amird <amir.dantas@hotmail.com>
2019-06-11 02:05:41 -0300

    Criação do comudlo de shared e core, e também criação da pasta de login (esta vazia)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,8 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: './list/list.module#ListPageModule'
-  }
+  },  { path: 'login', loadChildren: './auth/pages/login/login.module#LoginPageModule' }
+
 ];
 
 @NgModule({
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { BrowserModule } from '@angular/platform-browser';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { RouteReuseStrategy } from '@angular/router';
+
+@NgModule({
+  declarations: [],
+  imports: [IonicModule.forRoot()],
+  exports: [BrowserModule, IonicModule],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ]
+})
+export class CoreModule {}
