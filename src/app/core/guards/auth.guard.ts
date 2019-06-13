30afcf7e2a5b343f52515354f1add77559968b77
amird <amir.dantas@hotmail.com>
2019-06-12 23:32:27 -0300

    a40

---
+++ src/app/core/guards/auth.guard.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+  Route,
+  UrlSegment,
+  CanLoad
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { take, tap } from 'rxjs/operators';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+    return this.checkAuthState(state.url);
+  }
+
+  canActivateChild(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+    return this.canActivate(route, state);
+  }
+
+  canLoad(route: Route, segments: UrlSegment[]): Observable<boolean> {
+    const url = segments.map(s => '/${s}').join('');
+    return this.checkAuthState(url).pipe(take(1));
+  }
+
+  private checkAuthState(redirect: string): Observable<boolean> {
+    return this.authService.isAuthenticated.pipe(
+      tap(is => {
+        if (!is) {
+          this.router.navigate(['/login'], {
+            queryParams: { redirect }
+          });
+        }
+      })
+    );
+  }
+}
