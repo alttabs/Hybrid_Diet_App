32b8d893e7edab7c7e133d72987b3ec2670a7410
amird <amir.dantas@hotmail.com>
2019-06-15 03:42:36 -0300

    criei o formulario somente e está comentado a outra forma de criação de formulario em dados.page.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [CoreModule, AppRoutingModule],
+  imports: [CoreModule, AppRoutingModule, FormsModule],
 
   bootstrap: [AppComponent]
 })
--- src/app/dados/dados.page.ts
+++ src/app/dados/dados.page.ts
@@ -9,50 +9,26 @@ import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms'
   styleUrls: ['./dados.page.scss']
 })
 export class DadosPage implements OnInit {
-  //GroupForm: FormGroup;
+  formulario: FormGroup;
+  cabeca: number;
 
 
   constructor(
     private navCtrl: NavController,
     private overlayService: OverlayService,
-   //private fb: FormBuilder
+    //private fb: FormBuilder
   ) {}
 
   ngOnInit() {
-    //this.createForm();
-  }
+    this.formulario = new FormGroup({
+      peso: new FormControl(''),
+    });
 
-  private createForm(): void {
-   /* this.GroupForm = this.fb.group({
-      peso: ['', [Validators.required, Validators.maxLength(3)]],
-      altura: ['', [Validators.required, Validators.minLength(3)]],
-      idade: ['', [Validators.required, Validators.minLength(2)]]
-     // sexo: ['', [Validators.required, Validators.minLength(3)]]
+    /*this.formulario = this.fb.group({
+      peso: [''],
     });*/
   }
 
- /* get peso(): FormControl {
-    return <FormControl>this.GroupForm.get('peso');
-  }
-
-  get altura(): FormControl {
-    return <FormControl>this.GroupForm.get('altura');
-  }
-
-  get idade(): FormControl {
-    return <FormControl>this.GroupForm.get('idade');
-  }
-*/
-
-
-
-    //cabeca = this.idade.value + this.altura.value;
-
-
-
-  /*get sexo(): FormControl {
-    return <FormControl>this.GroupForm.get('sexo');
-  }*/
 
   async onClick(): Promise<void> {
     const loading = await this.overlayService.loading();
