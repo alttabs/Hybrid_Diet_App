810ca62794e6b9d4d9fb51bf8cd731329c377e75
amird <amir.dantas@hotmail.com>
2019-06-17 01:15:33 -0300

    habilitar lohin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'dados',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
 
